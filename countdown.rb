7831d6c650c6acb1b374803088e3ef5a0e755a04
Rachneet <rachneetsethi@gmail.com>
2018-12-03 02:08:21 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,9 @@
-#write your code here
-
-def countdown
+def countdown 
+  countdown = 10 
+  while countdown < 10 
+  puts "#{countdown} Second (S)!" 
+  countdown +- 1
+  sleep (1)
 end
+
+puts "HAPPY NEW YEAR!"
